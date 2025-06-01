7f94a337d52649cda87bd0d447dfa13a3540a536
Swetha Sukumar <swethasukumar14@gmail.com>
2025-06-01 15:13:44 -0400

    [Swetha] Implement top-level text node extraction

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [textNodes, setTextNodes] = useState<string[]>([]);
+
+  useEffect(() => {
+
+    if (containerRef.current) {
+      const nodes = containerRef.current.childNodes
+      const texts: string[] = []
+
+
+      nodes.forEach((node) => {
+        if (node.nodeType === 3) {
+
+          const text = node.textContent?.trim();
+          if (text) {
+            texts.push(text)
+          }
+        }
+      })
+
+      setTextNodes(texts)
+
+    }
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World!</p>
-      </header>
+      <div ref={containerRef}>
+        Hello this is some text
+        <p>this is paragrph text</p>
+        <span>this is span text</span>
+        this is last text
+      </div>
+
+      <div>
+        <ul>
+          {textNodes.map((text, idx) => (
+            <li key={idx}>{text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
