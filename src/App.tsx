c37e9487244d0b560322cc1946815662bc1f4d60
Swetha Sukumar <swethasukumar14@gmail.com>
2025-06-02 10:42:09 -0400

    [Swetha] Validate User Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { useDebounce } from './useDebounce';
-import { StarRating } from './StarRating';
 
 function App() {
-  const [rating, setRating] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const input = e.target.value
+    setInputValue(input)
+
+    if (input.length <= 5) {
+      setError("Must contain more than 5 characters")
+    } else {
+      setError("")
+    }
+
+  }
+
 
   return (
     <div className="App" style={{ marginTop: 500 }}>
 
-      <h3>Your Rating: {rating}</h3>
-      <StarRating max={5} onChange={setRating} />
+      <label>
+        Text Input: <input value={inputValue} name="myInput" onChange={handleChange} />
+      </label>
+
+      {error && <span>{error}</span>}
     </div>
   );
 }
